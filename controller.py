2c8574f3826ff6eb2fb637cd1408be7d7924f13b
Paolo Aimar <s297424@studenti.polito.it>
2024-03-29 10:55:36 +0100

    Indovina numero completato, ancora da controllare come lo risolve il docente.

--- controller.py
+++ controller.py
@@ -1,9 +1,61 @@
+import random
+
 from view import View
 from model import Model
 import flet as ft
-
 
 class Controller(object):
     def __init__(self, view: View):
         self._view = view
         self._model = Model()
+
+    def handleNewGame(self, e):
+        self._view._numeroMax.value = f"{random.randint(1, 150)}"
+        tentativi = random.randint(4, 10)
+        self._view._indovina.read_only = False
+        self._view._TentativiRimanenti.value = f"{tentativi}"
+        self._view._TentativiMax.value = f"{tentativi}"
+        self._view._btnIndovina.disabled = False
+        self._view._numeroCasuale = random.randint(1, int(self._view._numeroMax.value))
+        self._view._indovina.value = ""
+        self._view._lvOut.controls.clear()
+        self._view.update()
+
+    def handleIndovina(self, e):
+        try:
+            indovina = int(self._view._indovina.value)
+        except ValueError:
+            self._view._lvOut.controls.append(ft.Text(value="il valore inserito deve essere un numero", color="red"))
+            self._view._indovina.value = ""
+            self._view.update()
+            return
+
+        if indovina == int(f"{self._view._numeroCasuale}"):
+            self._view._lvOut.controls.append(ft.Text(f"Grande! il numero segreto era {self._view._numeroCasuale}"))
+            self._view._indovina.read_only = True
+            self._view._btnIndovina.disabled = True
+            self._view.update()
+            return
+
+        elif indovina < int(f"{self._view._numeroCasuale}"):
+            self._view._lvOut.controls.append(ft.Text(f"il numero segreto è più grande di {self._view._indovina.value}"))
+            self._view._indovina.value = ""
+            self._view._TentativiRimanenti.value = str(int(self._view._TentativiRimanenti.value) - 1)
+            if self._view._TentativiRimanenti.value == "0":
+                self._view._lvOut.controls.append(ft.Text(f"Hai finito i tentativi, mi spiace hai perso!", color="blue"))
+                self._view._indovina.read_only = True
+                self._view._btnIndovina.disabled = True
+                self._view.update()
+                return
+
+        else:
+            self._view._lvOut.controls.append(ft.Text(f"il numero segreto è più piccolo di {self._view._indovina.value}"))
+            self._view._indovina.value = ""
+            self._view._TentativiRimanenti.value = str(int(self._view._TentativiRimanenti.value) - 1)
+            if self._view._TentativiRimanenti.value == "0":
+                self._view._lvOut.controls.append(ft.Text(f"Hai finito i tentativi, mi spiace hai perso!", color="blue"))
+                self._view._indovina.read_only = True
+                self._view._btnIndovina.disabled = True
+                self._view.update()
+                return
+        self._view.update()
--- view.py
+++ view.py
@@ -1,3 +1,5 @@
+import random
+
 import flet as ft
 
 
@@ -14,9 +16,37 @@ class View(object):
                                color="blue", size=24)
 
         self._page.add(self._titolo)
+        self._numeroMax = ft.TextField(label="Numero Max",
+                                       value=f"{random.randint(1, 150)}",
+                                       read_only=True,
+                                       width=150)
+        self._btnStartGame = ft.ElevatedButton(text="Nuova Partita",
+                                               width=150,
+                                               on_click=self._controller.handleNewGame)
+        row1 = ft.Row([self._numeroMax, self._btnStartGame],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.add(row1)
+
+        tentativi = random.randint(4, 10)
+        self._TentativiRimanenti = ft.TextField(label="Tentativi Rimanenti", value=f"{tentativi}", width=150)
+        self._TentativiMax = ft.TextField(label="Tentativi Max",
+                                       value=f"{tentativi}",
+                                       read_only=True,
+                                       width=150)
+        row2 = ft.Row([self._TentativiRimanenti, self._TentativiMax], alignment=ft.MainAxisAlignment.CENTER)
+        self._page.add(row2)
+        self._numeroCasuale = random.randint(1, int(self._numeroMax.value))
+        self._indovina = ft.TextField(hint_text="Valore", width=150)
+        self._btnIndovina = ft.ElevatedButton(text="Indovina", on_click=self._controller.handleIndovina, width=150)
+        self._lvOut = ft.ListView()
+        self._lvOut.controls.append(ft.Text("indovina a quale numero sto pensando: "))
+        row3 = ft.Row([self._indovina, self._btnIndovina], alignment=ft.MainAxisAlignment.CENTER)
+        self._page.add(row3)
+        self._page.add(self._lvOut)
 
     def setController(self, controller):
         self._controller = controller
 
     def update(self):
         self._page.update()
+
